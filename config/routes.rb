4ea05228a8a5d7dacdaa9a797379261453832eac
Robyn <hellothisisrobyn@gmail.com>
2025-06-06 02:25:50 -0400

    set up routes and resources for candidates and votes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,21 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Session (login) routes
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # Unauthenticated "results" dashboard
+  get '/results', to: 'results#index'
+
+  # All the "voting" endpoints under /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :candidates,  only: [:index, :create] # only need to list all and write in a candidate
+      resources :votes,       only: [:create] # only need to cast a vote
+    end
+  end
   # Defines the root path route ("/")
   root "home#index"
 end
